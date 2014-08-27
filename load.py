7bc48ab1610f453e1f33d8e57de026dccf93c94c
Noah Seger <nosamanuel@gmail.com>
2014-08-27 17:21:11 -0500

    Load ZIP codes with missing ZCTAs

--- load.py
+++ load.py
@@ -4,8 +4,9 @@ import csv
 import os
 
 from django.conf import settings
-from django.contrib.gis.geos import Point
+from django.contrib.gis.geos import Point, MultiPolygon, Polygon
 from django.contrib.gis.utils import LayerMapping
+from django_localflavor_us.forms import USStateField
 
 from places.models import Community, Market, PostalCode
 
@@ -29,7 +30,7 @@ class ZIPCodeMapping(LayerMapping):
         kwargs.setdefault('unique', 'postal_code')
         kwargs.setdefault('encoding', 'latin-1')
         mapping = mapping or self.mapping
-        self.zip_code_data = kwargs.pop('zip_code_data')
+        self.zip_code_data = dict(kwargs.pop('zip_code_data'))
         super(ZIPCodeMapping, self).__init__(model, data, mapping, **kwargs)
 
     def feature_kwargs(self, feature):
@@ -42,6 +43,8 @@ class ZIPCodeMapping(LayerMapping):
         number = kwargs['postal_code']
         zip_code = str(number).zfill(5)
         extra_kwargs = self.zip_code_data[zip_code]
+        extra_kwargs.pop('latitude', None)
+        extra_kwargs.pop('longitude', None)
         extra_kwargs.update(
             id=number,
             center=center,
@@ -93,6 +96,28 @@ def load_places():
     mapping = ZIPCodeMapping(PostalCode, zip_shp, zip_code_data=zip_code_data)
     mapping.save()
 
+    # Create any ZIP codes that were missing from the Census tabulation.
+    # MySQL can't index nullable geometry columns so we insert them with
+    # empty tabulations and (0.0, 0.0) centers where necessary.
+    polygon = Polygon.from_bbox(((0, 0, 0, 0)))
+    tabulation = MultiPolygon(polygon)
+    postal_codes = PostalCode.objects.all()
+    all_zip_codes = set(postal_codes.values_list('postal_code', flat=True))
+    for zip_code, data in zip_code_data.iteritems():
+        data.setdefault('latitude', 0.0)
+        data.setdefault('longitude', 0.0)
+        if zip_code not in all_zip_codes:
+            PostalCode.objects.create(
+                id=int(zip_code),
+                postal_code=zip_code,
+                market_id=data['market_id'],
+                community_id=data['community_id'],
+                center=Point(data['longitude'], data['latitude']),
+                tabulation=tabulation,
+                state=data['state'],
+                country='US'
+            )
+
 
 def read_owcp():
     """
@@ -103,7 +128,12 @@ def read_owcp():
         utf8_stream = read_utf8(stream)
         reader = csv.DictReader(utf8_stream)
         for data in reader:
-            yield data['ZIP CODE'], data['MSA No.'], data['MSA Name']
+            zip_code = str(int(float(data['ZIP CODE']))).zfill(5)
+            yield zip_code, dict(
+                msa_id=data['MSA No.'],
+                msa_name=data['MSA Name'],
+                state=data['STATE']
+            )
 
 
 def read_geonames():
@@ -115,8 +145,15 @@ def read_geonames():
         utf8_stream = read_utf8(stream)
         reader = csv.reader(utf8_stream)
         for row in reader:
-            _, zip_code, _, _, state, community_name, community_id = row[:7]
-            yield zip_code, state, community_id, community_name
+            (_, zip_code, _, _, state, community_name, community_id,
+             _, _, latitude, longitude) = row[:11]
+            yield zip_code, dict(
+                state=state,
+                community_id=community_id,
+                community_name=community_name,
+                latitude=float(latitude),
+                longitude=float(longitude)
+            )
 
 
 def get_place_data():
@@ -127,18 +164,11 @@ def get_place_data():
     """
     zip_code_data = collections.defaultdict(dict)
 
-    for zip_code, msa_id, msa_name in read_owcp():
-        zip_code_data[zip_code].update(
-            msa_id=msa_id,
-            msa_name=msa_name
-        )
+    for zip_code, owcp_data in read_owcp():
+        zip_code_data[zip_code].update(owcp_data)
 
-    for zip_code, state, community_id, community_name in read_geonames():
-        zip_code_data[zip_code].update(
-            state=state,
-            community_id=community_id,
-            community_name=community_name
-        )
+    for zip_code, geonames_data in read_geonames():
+        zip_code_data[zip_code].update(geonames_data)
 
     community_data, market_data = dict(), dict()
     for zip_code, data in zip_code_data.iteritems():
